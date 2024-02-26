47a4da64f0245fd4f3593d8e9b88d038291471f8
Nikhil Jha <nikhiljhanov15@gmail.com>
2024-02-26 22:39:58 +0530

    Add link to Coins Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <div className="font-bold text-4xl">heloo</div>
+    <div className="font-bold text-4xl flex justify-center items-center h-screen">
+      <Link href="/home">Go To Coins Page  -{'>'}</Link>
+    </div>
     </>
   );
 }
